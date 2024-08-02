b3f55ce6543f9cceef349230c0f0b32ea72b7e8f
harshvaghanii <harshvaghani98@gmail.com>
2024-08-02 17:38:44 -0400

    adding entities, using ORM and also using H2 database to store data

--- src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
+++ src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
@@ -1,29 +1,44 @@
 package com.codingshuttle.springbootwebtutorial.springbootwebtutorial.controllers;
 
 import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.dto.EmployeeDTO;
+import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.entities.EmployeeEntity;
+import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.repositories.EmployeeRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
 
+    private final EmployeeRepository repository;
+
+    public EmployeeController(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping
-    public String getAllEmployees(@RequestParam(required = false) Integer age, @RequestParam(required = false) String sortBy) {
-        return "Hi " + age + " " + sortBy;
+    public List<EmployeeEntity> getAllEmployees(@RequestParam(required = false) Integer age, @RequestParam(required = false) String sortBy) {
+        return repository.findAll();
     }
 
     @GetMapping("/{employeeID}")
-    public EmployeeDTO getEmployeeByID(@PathVariable String employeeID) {
-        return new EmployeeDTO(Long.parseLong(employeeID), "Harsh", "hvaghani@etech.medallia.com", 25, LocalDate.of(2021, 1, 18), false);
+    public EmployeeEntity getEmployeeByID(@PathVariable String employeeID) {
+        Long id = Long.parseLong(employeeID);
+        return repository.findById(id).orElse(null);
     }
 
     @PostMapping
-    public EmployeeDTO addEmployee(@RequestBody EmployeeDTO employee) {
-        employee.setId(123L);
-        return employee;
+    public EmployeeEntity addEmployee(@RequestBody EmployeeEntity employee) {
+        return repository.save(employee);
+    }
+
+    @DeleteMapping("/{employeeID}")
+    public void deleteEmployee(@PathVariable String employeeID) {
+        Long id = Long.parseLong(employeeID);
+        repository.deleteById(id);
     }
 
 }
