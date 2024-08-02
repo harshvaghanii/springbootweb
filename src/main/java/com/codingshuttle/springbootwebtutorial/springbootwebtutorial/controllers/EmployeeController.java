85f0c04b248268bd5e688d4ae69725796e3638e2
harshvaghanii <harshvaghani98@gmail.com>
2024-08-01 22:01:29 -0400

    adding requests for post method and testing it on postman

--- src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
+++ src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
@@ -1,30 +1,28 @@
 package com.codingshuttle.springbootwebtutorial.springbootwebtutorial.controllers;
 
 import com.codingshuttle.springbootwebtutorial.springbootwebtutorial.dto.EmployeeDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.Date;
 
 @RestController
+@RequestMapping("/employees")
 public class EmployeeController {
 
-    @GetMapping("/")
-    public String home() {
-        return "Welcome to my Spring Boot Application!";
+    @GetMapping
+    public String getAllEmployees(@RequestParam(required = false) Integer age, @RequestParam(required = false) String sortBy) {
+        return "Hi " + age + " " + sortBy;
     }
 
-    @GetMapping("/employees/{employeeID}")
+    @GetMapping("/{employeeID}")
     public EmployeeDTO getEmployeeByID(@PathVariable String employeeID) {
         return new EmployeeDTO(Long.parseLong(employeeID), "Harsh", "hvaghani@etech.medallia.com", 25, LocalDate.of(2021, 1, 18), false);
     }
 
-    @GetMapping(path = "/employees")
-    public String getAllEmployees(@RequestParam(required = false) Integer age, @RequestParam(required = false) String sortBy) {
-        return "Hi " + age + " " + sortBy;
+    @PostMapping
+    public String addEmployee() {
+        return "Hello from Post method!";
     }
 
 }
